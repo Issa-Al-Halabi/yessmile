46e8337a99aea2d09bdef563fb13544e53bfffef
Issa Al-Halabi <eesahalabi@gmail.com>
2024-03-09 20:39:16 +0300

    add delete button in news resource

--- app/Filament/Resources/NewsResource.php
+++ app/Filament/Resources/NewsResource.php
@@ -12,6 +12,7 @@ use App\Models\Category;
 use Filament\Forms;
 use Filament\Resources\Concerns\Translatable;
 use Filament\Forms\Components\Tabs;
+
 class NewsResource extends Resource
 {
     use Translatable;
@@ -26,75 +27,75 @@ class NewsResource extends Resource
         return $form
             ->schema([
                 Tabs::make('Tabs')
-                ->tabs([
-                    Tabs\Tab::make('General Informations')
-                        ->icon('heroicon-o-chat-bubble-bottom-center-text')
-                        ->schema([
+                    ->tabs([
+                        Tabs\Tab::make('General Informations')
+                            ->icon('heroicon-o-chat-bubble-bottom-center-text')
+                            ->schema([
 
-                            Forms\Components\TextInput::make('title')
-                                ->label("Title")
-                                ->required()
-                                ->maxLength(255),
-                            Forms\Components\DateTimePicker::make('date')
-                                ->label("Date")
-                                ->default(now())
-                                ->required(),
-                            Forms\Components\Textarea::make('short_description')
-                                ->label("Short Description")
-                                ->required()
-                                ->columnSpanFull(),
-                            Forms\Components\RichEditor::make('long_description')
-                                ->label("Long Description")
-                                ->required()
-                                ->columnSpanFull(),
-                        ])->columns(3),
-                    Tabs\Tab::make('News Image')
-                        ->icon('heroicon-o-document-arrow-up')
-                        ->schema([
-                            Forms\Components\FileUpload::make('image')
-                                ->label("Image")
-                                ->image()
-                                ->required()
-                                ->columnSpan('full')
-                                ->nullable(false)
-                                ->disk('public')
-                                ->directory('articles')
-                                ->visibility('public')
-                                ->imageResizeMode('force')
-                                ->imageCropAspectRatio('8:5')
-                                ->imageResizeTargetWidth('800')
-                                ->imageResizeTargetHeight('500')
-                                ->imageEditor(),
-                        ]),
-                    Tabs\Tab::make('socials')
-                        ->icon('heroicon-o-at-symbol')
-                        ->schema([
-                            Forms\Components\Repeater::make('socials')
-                                ->defaultItems(0)
-                                ->schema([
-                                    Forms\Components\FileUpload::make('image')
-                                        ->label("Image")
-                                        ->image()
-                                        ->required()
-                                        ->columnSpan('full')
-                                        ->nullable(false)
-                                        ->disk('public')
-                                        ->directory('articles')
-                                        ->visibility('public')
-                                        ->imageResizeMode('force')
-                                        ->imageCropAspectRatio('8:5')
-                                        ->imageResizeTargetWidth('800')
-                                        ->imageResizeTargetHeight('500')
-                                        ->imageEditor(),
-                                    Forms\Components\TextInput::make('link')
-                                        ->label("Link")
-                                        ->url()
-                                        ->required()
-                                        ->maxLength(255),
-                                ]),
-                        ]),
-                ])
-                ->columnSpan('full'),
+                                Forms\Components\TextInput::make('title')
+                                    ->label("Title")
+                                    ->required()
+                                    ->maxLength(255),
+                                Forms\Components\DateTimePicker::make('date')
+                                    ->label("Date")
+                                    ->default(now())
+                                    ->required(),
+                                Forms\Components\Textarea::make('short_description')
+                                    ->label("Short Description")
+                                    ->required()
+                                    ->columnSpanFull(),
+                                Forms\Components\RichEditor::make('long_description')
+                                    ->label("Long Description")
+                                    ->required()
+                                    ->columnSpanFull(),
+                            ])->columns(3),
+                        Tabs\Tab::make('News Image')
+                            ->icon('heroicon-o-document-arrow-up')
+                            ->schema([
+                                Forms\Components\FileUpload::make('image')
+                                    ->label("Image")
+                                    ->image()
+                                    ->required()
+                                    ->columnSpan('full')
+                                    ->nullable(false)
+                                    ->disk('public')
+                                    ->directory('articles')
+                                    ->visibility('public')
+                                    ->imageResizeMode('force')
+                                    ->imageCropAspectRatio('8:5')
+                                    ->imageResizeTargetWidth('800')
+                                    ->imageResizeTargetHeight('500')
+                                    ->imageEditor(),
+                            ]),
+                        Tabs\Tab::make('socials')
+                            ->icon('heroicon-o-at-symbol')
+                            ->schema([
+                                Forms\Components\Repeater::make('socials')
+                                    ->defaultItems(0)
+                                    ->schema([
+                                        Forms\Components\FileUpload::make('image')
+                                            ->label("Image")
+                                            ->image()
+                                            ->required()
+                                            ->columnSpan('full')
+                                            ->nullable(false)
+                                            ->disk('public')
+                                            ->directory('articles')
+                                            ->visibility('public')
+                                            ->imageResizeMode('force')
+                                            ->imageCropAspectRatio('8:5')
+                                            ->imageResizeTargetWidth('800')
+                                            ->imageResizeTargetHeight('500')
+                                            ->imageEditor(),
+                                        Forms\Components\TextInput::make('link')
+                                            ->label("Link")
+                                            ->url()
+                                            ->required()
+                                            ->maxLength(255),
+                                    ]),
+                            ]),
+                    ])
+                    ->columnSpan('full'),
 
             ]);
     }
@@ -103,37 +104,38 @@ class NewsResource extends Resource
     {
         return $table
             ->columns([
-            Tables\Columns\TextColumn::make('title')
-                ->label("Title")
-                ->searchable(),
-            Tables\Columns\ImageColumn::make('image'),
-            Tables\Columns\ImageColumn::make('socials.0.image')
-                ->label("socials")
-                ->getStateUsing(function (News $record) {
-                    $images = [];
-                    foreach ($record->socials as $social) {
-                        $images[] = $social["image"];
-                    }
-                    return $images;
-                }),
-            Tables\Columns\TextColumn::make('date')
-                ->label("Date")
-                ->dateTime()
-                ->sortable(),
-            Tables\Columns\TextColumn::make('created_at')
-                ->dateTime()
-                ->sortable()
-                ->toggleable(isToggledHiddenByDefault: true),
-            Tables\Columns\TextColumn::make('updated_at')
-                ->dateTime()
-                ->sortable()
-                ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('title')
+                    ->label("Title")
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\ImageColumn::make('socials.0.image')
+                    ->label("socials")
+                    ->getStateUsing(function (News $record) {
+                        $images = [];
+                        foreach ($record->socials as $social) {
+                            $images[] = $social["image"];
+                        }
+                        return $images;
+                    }),
+                Tables\Columns\TextColumn::make('date')
+                    ->label("Date")
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
