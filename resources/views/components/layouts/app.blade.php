088895e0aa4a871e707af2201e26ee3dd2915427
Issa Al-Halabi <eesahalabi@gmail.com>
2024-03-09 20:25:56 +0300

    remove rtl app

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr" @endif
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl" @endif
 class="scroll-smooth">
 
 <!-- Mirrored from yessmile.ae/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 03 Mar 2024 14:35:58 GMT -->
